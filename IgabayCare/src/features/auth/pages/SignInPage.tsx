e0313fcbc1d16af81dcea6edd4c7410cabfb268b
Query <rexpagatpat@gmail.com>
2025-07-27 18:51:05 +0800

    Update SignInPage.tsx

--- IgabayCare/src/features/auth/pages/SignInPage.tsx
+++ IgabayCare/src/features/auth/pages/SignInPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { SignInForm } from '../components/SignInForm';
 
 const SignInPage: React.FC = () => {
