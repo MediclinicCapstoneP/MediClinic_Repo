983257623ad989b8cadca71740bf7ca859a01498
Query <rexpagatpat@gmail.com>
2025-08-16 23:51:12 +0800

    tryed the layout

---
+++ IgabayECare/src/components/Home.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="layout-menu">
+    <app-menu-item
+      v-for="(item, index) in layoutState.menuItems"
+      :key="index"
+      :item="item"
+      :index="index"
+      :root="true"
+    />
+  </div>
+</template>
