f552ccc53d0e0eb82f5c2f1780ec5c98dd92e0e2
Query <rexpagatpat@gmail.com>
2025-12-24 19:14:42 +0800

    go

--- IgabayCare/src/App.tsx
+++ IgabayCare/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import LearnMore from './pages/LearnMore';
 import SignInPage from './features/auth/pages/SignInPage';
